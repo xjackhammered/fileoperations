a838072cf84473b5df7ed8bbd350b39a29298b71
xjackhammered <159502632+xjackhammered@users.noreply.github.com>
2024-03-12 12:03:15 +0600

    Add files via upload

---
+++ fileops.py
@@ -0,0 +1,117 @@
+def read_file(file_name):
+    """ Reads in a file.
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable using the File read()
+           function
+        2. Print the contents of the file
+        3. Return the contents of the file
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        string: contents of the given file.
+    """
+    with open(file_name,"r") as file:
+        file_contents = file.read()
+        print(file_contents)
+        return file_contents
+    
+    raise NotImplementedError()
+
+def read_file_into_list(file_name):
+    """ Reads in a file and stores each line as an element in a list
+
+    [IMPLEMENT ME]
+        1. Open the given file
+        2. Read the file line by line and append each line to a list
+        3. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: a list where each element is a line in the file.
+    """
+    with open(file_name,"r") as file:
+        f_list = file.readlines()
+        return f_list
+
+    raise NotImplementedError()
+
+def write_first_line_to_file(file_contents, output_filename):
+    """ Writes the first line of a string to a file.
+
+    Args:
+        file_contents: string to be split and written into output file
+        output_filename: the name of the file to be written to
+    """
+    with open(output_filename, "w") as output_file:
+        output_file.write(file_contents.split("\n")[0])
+    
+
+
+
+def read_even_numbered_lines(file_name):
+    """ Reads in the even numbered lines of a file
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable
+        2. Read the file line-by-line and add the even-numbered lines to a list
+        3. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: a list of the even-numbered lines of the file
+    """
+    even_list = []
+    with open(file_name, "r") as file:
+        line_num = 0
+        for line in file:
+            line_num += 1
+            if line_num % 2 != 0:
+                even_list.append(line)
+        return even_list
+
+
+    raise NotImplementedError()
+
+def read_file_in_reverse(file_name):
+    """ Reads a file and returns a list of the lines in reverse order
+
+    [IMPLEMENT ME]
+        1. Open and read the given file into a variable
+        2. Read the file line-by-line and store the lines in a list in reverse order
+        3. Print the list
+        4. Return the list
+
+    Args:
+        file_name: the name of the file to be read
+
+    Returns:
+        list: list of the lines of the file in reverse order.
+    """
+    reversed_lines = []
+    with open(file_name, "r") as file:
+        for line in file:
+            reversed_lines.insert(0, line)
+    return reversed_lines
+
+    raise NotImplementedError()
+
+'''
+Here are some sample commands to help you run/test your implementations.
+Feel free to uncomment/modify/add to them as you wish.
+'''
+def main():
+    file_contents = read_file("sampletext.txt")
+    print(read_file_into_list("sampletext.txt"))
+    write_first_line_to_file(file_contents, "online.txt")
+    print(read_even_numbered_lines("sampletext.txt"))
+    print(read_file_in_reverse("sampletext.txt"))
+
+if __name__ == "__main__":
+    main()
